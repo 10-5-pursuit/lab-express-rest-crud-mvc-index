a0f1ea69b8c7fbc6c90a7e54dc946ccc001e619d
eivor9 <njeremiah@pursuit.org>
2024-06-21 14:22:05 -0400

    Complete bonus

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const locations = express.Router();
 const locationsArray = require("../models/location.model");
+const peopleArray = require("../models/person.model");
 
 locations.get("/", (req, res) => {
     res.json(locationsArray);
+})
+
+locations.get("/people", (req, res) => {
+    const locationsCopy = [...locationsArray]
+    for (const location of locationsCopy) {
+        location.people = peopleArray.filter(person => person.mainLocation === location.zip);
+    }
+    res.json(locationsCopy);
 })
 
 module.exports = locations;
