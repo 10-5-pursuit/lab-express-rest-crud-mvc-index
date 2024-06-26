668d517d2d0bd31944ef53328ab417029f690d54
Diamond <dbrown@pursuit.org>
2024-06-25 21:58:45 -0400

    Added some CRUD within some routes

--- controllers.js/locations.controller.js
+++ controllers.js/locations.controller.js
@@ -2,9 +2,12 @@
 const express = require('express');
 // create an istance of our express router 
 const locations = express.Router();
-// import the location model 
+// import the models info being used
 const locationsData = require('../models/location.model');
 const personsData = require('../models/person.model')
+
+// import the valisation function AKA MIDDLEWARE
+const { checkForNameKey } = require('../validations/locationsValidation');
 
 // create route that at goes to the corrct data file
 // Index route
@@ -13,37 +16,40 @@ locations.get('/', (req, res) => {
 });
 
 // show route 
-locations.get('/:arrayIndex', (req, res) => {
-    locationsData.map(locate => {
-        locate["people"] = [];
-        personsData.map(person => {
-            if (locate.zip === person.mainLocation) {
-                locate["people"].push(person);
-                // console.log(person)
-            }
-        });
-    });
-
-    res.json(locationsData);
-});
-
-// show route 
 // locations.get('/:arrayIndex', (req, res) => {
-//     const { arrayIndex } = req.params;
-//     if (locationsData[arrayIndex]) {
-//         res.json(locationsData[arrayIndex]);
-//     } else {
-//         res.json({ error: "Location not Found" });
-//     }
+//     locationsData.map(locate => {
+//         locate["people"] = [];
+//         personsData.map(person => {
+//             if (locate.zip === person.mainLocation) {
+//                 locate["people"].push(person);
+//                 // console.log(person)
+//             }
+//         });
+//     });
 
 //     res.json(locationsData);
 // });
 
+// show route 
+locations.get('/:arrayIndex', (req, res) => {
+    const { arrayIndex } = req.params;
+    if (locationsData[arrayIndex]) {
+        res.json(locationsData[arrayIndex]);
+    } else {
+        res.json({ error: "Location not Found" });
+    }
+
+    res.json(locationsData);
+});
+
 // create route
-// locations.post('/', (req, res)=>{
-//     locationsData.push(req.body);
-//     res.json(locationsData[locationsData.length-1]);
-// });
+locations.post('/', checkForNameKey, (req, res)=>{
+    locationsData.push(req.body);
+    res.json(locationsData[locationsData.length-1]);
+});
+
+
+
 
 
 
--- controllers.js/machines.controller.js
+++ controllers.js/machines.controller.js
@@ -4,12 +4,42 @@ const express = require('express');
 const machines = express.Router();
 // import the location model 
 const machinesData = require('../models/machine.model');
+const { checkForPurchasedKey } = require('../validations/machineValidation');
 
 
-// create route that at goes to the corrct data file
-machines.get('/', (req, res)=>{
+// Index route of all machine data
+machines.get('/', (req, res) => {
     res.json(machinesData);
-    });
+});
+
+// CREATE new car data
+// contains middleware
+// machines.post('/:car', checkForPurchasedKey, (req, res) => {
+//     machinesData.push(req.body);
+//     res.json(machinesData);
+
+// });
+
+// UPDATE route using the index from the url 
+// Yes, you can remove .status(200) without causing an error. By default, Express will use the status code 200 OK if you don't explicitly set a status code
+// machines.put('/:car', (req, res) => {
+//     const { car } = req.params;
+//     machinesData[car] = req.body;
+//     res.status(200).json(machinesData[car]);
+// });
+
+
+// delete route
+machines.delete('/:car', (req, res) => {
+    const { car } = req.params;
+    if (machinesData[car]) {
+        machinesData.splice(car, 1);
+        res.json({ message: "Successfully brought a car!" });
+    } else {
+        res.json({ error: "Car Not Found" });
+    }
+});
+
 
 //export router
 module.exports = machines;
